a24cf288d77648506dd3f5babb0a9171e6744320
César Bahena <cabahenac@gmail.com>
2025-12-19 09:59:02 -0600

    fix: use dominance instead of state in genotype traits schema

--- seeds/002_genotypes.sql
+++ seeds/002_genotypes.sql
@@ -1,2 +1,2 @@
 INSERT INTO genotypes (id, kind, name, generation, genome_id, traits, created_at) VALUES
-    ('00000000-0000-0000-0000-000000000002', 'FEAT', 'Feature Request', 1, '00000000-0000-0000-0000-000000000001', '[{"key": "title", "state": "Dominant"}, {"key": "description", "state": "Recessive"}]', NOW());
+    ('00000000-0000-0000-0000-000000000002', 'FEAT', 'Feature Request', 1, '00000000-0000-0000-0000-000000000001', '[{"key": "title", "dominance": "Dominant"}, {"key": "description", "dominance": "Recessive"}]', NOW());
